213b6d3c6eeb6019dcd2edf083349548c36c9adb
Teacher <Peezme021@gmail.com>
2024-05-02 14:40:28 +0700

    test

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,40 @@ public class Main
     public static void main(String[] args)
     {
         Scanner scanner = new Scanner(System.in);
+        System.out.print("input scroe ");
+        int score = scanner.nextInt();
 
-    }
+        if(score >= 80)
+        {
+            System.out.println("Grade 4 " +"\n"+score);
+        }
+        else if (score >= 75)
+        {
+            System.out.println("Grade 3.5 "+"\n"+score);
+        }
+        else if (score >= 70)
+        {
+            System.out.println("Grade 3 "+"\n"+score);
+        }
+        else if (score >= 65)
+        {
+            System.out.println("Grade 2.5 "+"\n"+score);
+        }
+        else if (score >= 60)
+        {
+        System.out.println("Grade 2 "+"\n"+score);
+        }
+        else if (score >= 55)
+        {
+            System.out.println("Grade 1.5 "+"\n"+score);
+        }
+        else if (score >= 50)
+        {
+            System.out.println("Grade 1 "+"\n"+score);
+        }
+        else
+        {
+            System.out.println("Grade 0 "+"\n"+score);
+        }
+}
 }
